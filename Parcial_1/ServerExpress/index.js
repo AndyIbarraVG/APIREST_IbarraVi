90a346cece1cb471c54fc4be58104bcd15621e8a
AndyIbarraVG <AndyIbarraVG@outlook.com>
2024-02-06 11:53:11 -0600

    Se modifico el archivo de Express

--- Parcial_1/ServerExpress/index.js
+++ Parcial_1/ServerExpress/index.js
@@ -3,20 +3,20 @@ const morgan = require('morgan');
 const app = express();
 
 // Función middleware propia
-app.use(req, res, next) => {
+app.use((req, res, next) => {
     console.log('Solicitud recibida: ' + req.url + ' por el método: ' + req.method);
     next();
-}
+});
 
 // Función middleware de terceros
 // app.use(morgan('combined'));
 
-app.get('/', (req, res,next) => {
-res.send('Contestando una petición GET desde Express');
+app.get('/', (req, res, next) => {
+    res.send('Contestando una petición GET desde Express');
 });
 
-app.post('/', (req, res,next) => {
-res.send('Contestando una petición POST desde Express');
+app.post('/', (req, res, next) => {
+    res.send('Contestando una petición POST desde Express');
 });
 
 app.listen(3000, () => {
