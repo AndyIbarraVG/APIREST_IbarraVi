bdfd125b43b1e92a381a24e776a965bf856f60fe
AndyIbarraVG <AndyIbarraVG@outlook.com>
2024-02-06 11:52:16 -0600

    Se modifico la carpeta Express

--- Parcial_1/ServerExpress/index.js
+++ Parcial_1/ServerExpress/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/', (req, res) => {
+// Función middleware propia
+app.use(req, res, next) => {
+    console.log('Solicitud recibida: ' + req.url + ' por el método: ' + req.method);
+    next();
+}
+
+// Función middleware de terceros
+// app.use(morgan('combined'));
+
+app.get('/', (req, res,next) => {
 res.send('Contestando una petición GET desde Express');
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res,next) => {
 res.send('Contestando una petición POST desde Express');
 });
 
 app.listen(3000, () => {
-console.log('Example app listening on port 3000!');
+console.log('servidor escuchando en http://localhost:3000/');
 });
