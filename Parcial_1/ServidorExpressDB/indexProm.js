8679bc348c875880937c89300778745a89ea03ee
AndyIbarraVG <AndyIbarraVG@outlook.com>
2024-02-21 08:28:54 -0600

    Se modifico el archivo indexProm.js

--- Parcial_1/ServidorExpressDB/indexProm.js
+++ Parcial_1/ServidorExpressDB/indexProm.js
@@ -36,18 +36,50 @@ app.listen(3000, () => {
     console.log('Servidor iniciado en http://localhost:3000');
 });
 
-// app.post('/pokemons', async (req, res) => {
-//     console.log(req.body);
-//     const { nombre, tipo, debilidad } = req.body;
-//     const sql = `INSERT INTO pokemons (nombre, tipo, debilidad) VALUES ('${nombre}', '${tipo}', '${debilidad}')`;
-//     try {
-//         const connection = await mysql.createConnection({
-//             host: 'localhost', user: 'root', database: 'Pokemon'
-//         });
-//         var [rows, fields] = await connection.execute(sql);
-//         res.json({ status: 'Pokemon guardado' });
-//     }
-//     catch (error) {
-//         console.log(error);
-//     }
-// });
+app.post('/pokemons', async (req, res) => {
+    console.log(req.body);
+    const { nombre, tipo, naturaleza,habilidad } = req.body;
+    const sql = `INSERT INTO pokemons (id_pokemon,Nombre, Tipo, Naturaleza, Habilidad) VALUES ('NULL','${nombre}', '${tipo}', '${naturaleza}','${habilidad}')`;
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost', user: 'root', database: 'Pokemon'
+        });
+        var [rows, fields] = await connection.execute(sql);
+        res.json({ status: 'Pokemon guardado' });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+app.put('/pokemons', async (req, res) => {
+    console.log(req.body);
+    const { id_pokemon, nombre, tipo, naturaleza,habilidad } = req.body;
+    const sql = `UPDATE pokemons SET Nombre = '${nombre}', Tipo = '${tipo}', Naturaleza = '${naturaleza}', Habilidad = '${habilidad}' WHERE id_pokemon = ${id_pokemon}`;
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost', user: 'root', database: 'Pokemon'
+        });
+        var [rows, fields] = await connection.execute(sql);
+        res.json({ status: 'Pokemon actualizado' });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+
+app.delete('/pokemons', async (req, res) => {
+    console.log(req.body);
+    const { id_pokemon } = req.body;
+    const sql = `DELETE FROM pokemons WHERE id_pokemon = ${id_pokemon}`;
+    try {
+        const connection = await mysql.createConnection({
+            host: 'localhost', user: 'root', database: 'Pokemon'
+        });
+        var [rows, fields] = await connection.execute(sql);
+        res.json({ status: 'Pokemon eliminado' });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
